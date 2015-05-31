524c6c5b2e9074076363066308ab3793416070c4
truyenminhtoan <truyenminhtoan@gmail.com>
2015-05-30 21:23:47 -0700

    test commit

--- HelloWorld/ViewController.m
+++ HelloWorld/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    UILabel *lb=[[UILabel alloc]initWithFrame:CGRectMake(10, 10, 10, 10)];
+    lb.text=@"ALO";
 }
 
 - (void)didReceiveMemoryWarning {
